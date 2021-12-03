a3543d269ff33c5d07f1182cc01f23fd707fc152
Abu Sayem <78730784+BinaryBeast007@users.noreply.github.com>
2021-12-03 22:38:36 +0600

    Added cycle detection in directed graph using dfs

---
+++ Graph Theory/Cycle Detection/detect_cycle_in_directed_graph_using_dfs.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> adj;
+vector<bool> visited, dfsVisited;
+
+bool checkForCycle(int node) {
+    visited[node] = true;
+    dfsVisited[node] = true;
+    for(auto it : adj[node]) {
+        if(!visited[it]) {
+            if(checkForCycle(it)) 
+                return true;
+        }else if(dfsVisited[it]) {
+            return true;
+        }
+    }
+    dfsVisited[node] = false;
+    return false;
+}
+
+bool isCyclic() {
+    int n = adj.size();
+    for(int i=0; i<n; i++) {
+        if(!visited[i]) {
+            if(checkForCycle(i)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    adj.assign(n, vector<int>());
+    visited.assign(n, false);
+    dfsVisited.assign(n, false);
+
+    for(int i=0; i<m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+    
+    if(isCyclic()) {
+        cout << "Graph contains cycle\n";
+    }else {
+        cout << "Graph doesn't contain cycle\n";
+    }
+
+    return 0;
+}
