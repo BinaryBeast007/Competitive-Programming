59ad4a996cc4a4ad5e7a4ae6aa4bdca27711c48b
Abu Sayem <78730784+BinaryBeast007@users.noreply.github.com>
2021-12-04 21:34:19 +0600

    Added topological sort

---
+++ Graph Theory/Topological Sort/topological_sort_using_dfs.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<vector<int>> adj;
+vector<bool> visited;
+vector<int> topo;
+
+void findTopoSort(int node, stack<int>& stk) {
+    visited[node] = true;
+    for(auto& it : adj[node]) {
+        if(!visited[it]) {
+            findTopoSort(it, stk);
+        }
+    }
+    stk.push(node);
+}
+
+void topoSort() {
+    stack<int> stk;
+    int n = adj.size();
+    for(int i=0; i<n; i++) {
+        if(!visited[i]) {
+            findTopoSort(i, stk);
+        }
+    }
+    while(!stk.empty()) {
+        topo.push_back(stk.top());
+        stk.pop();
+    }
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    adj.assign(n, vector<int>());
+    visited.assign(n, false);
+
+    for(int i=0; i<m; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    topoSort();
+    for(auto& i : topo) {
+        cout << i << " ";
+    }
+
+    return 0;
+}
