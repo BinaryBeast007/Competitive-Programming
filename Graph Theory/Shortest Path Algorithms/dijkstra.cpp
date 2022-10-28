668bf551254de990891d45a42d59a1ffb5ee0e04
Abu Sayem <78730784+BinaryBeast007@users.noreply.github.com>
2022-10-28 21:32:29 +0600

    Updated dijkstra

--- Graph Theory/Shortest Path Algorithms/dijkstra.cpp
+++ Graph Theory/Shortest Path Algorithms/dijkstra.cpp
@@ -1,52 +1,64 @@
 #include<bits/stdc++.h>
-using namespace std;
+using namespace std;      
 
-vector<vector<pair<int, int>>> adj;
-vector<int> dist;
-vector<bool> visited;
+const int N = 1e5 + 5, INF = 1e9 + 10;
+vector<pair<int, int>> g[N];
+vector<int> dist, parent;
 
-void dijkstra(int src) {
-    // priority_queue<dist, node>
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+void Dijkstra(int src) {
+    dist.assign(N, INF);
+    parent.assign(N, -1);
     dist[src] = 0;
-    pq.push({dist[src], src});
 
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({0, src});
     while(!pq.empty()) {
-        int distance = pq.top().first;
-        int node = pq.top().second;
+        int node = pq.top().second, d_node = pq.top().first;
         pq.pop();
-        if(visited[node]) continue;
-        visited[node] = true;
-
-        for(auto& it : adj[node]) {
-            int next = it.first;
-            int nextDist = it.second;
-            if(distance + nextDist < dist[next]) {
-                dist[next] = distance + nextDist;
-                pq.push({dist[next], next});
+        if(d_node != dist[node]) {
+            continue;
+        }
+        for(auto& edge : g[node]) {
+            int child = edge.first, w = edge.second;
+            if(dist[node] + w < dist[child]) {
+                dist[child] = dist[node] + w;
+                parent[child] = node;
+                pq.push({dist[child], child});
             }
         }
     }
 }
 
-int main() {
-    int n, m, src;
-    cin >> n >> m >> src;
-
-    adj.assign(n, vector<pair<int, int>>());
-    dist.assign(n, INT_MAX);
-    visited.assign(n, false);
-
-    for(int i=0; i<m; i++) {
-        int u, v, wt;
-        cin >> u >> v >> wt;
-        --u, --v;
-        adj[u].push_back({v, wt});
-        adj[v].push_back({u, wt});
+vector<int> get_path(int src, int to) {
+    vector<int> path;
+    for (int v = to; v != src; v = parent[v]) {
+        path.push_back(v);
     }
-    dijkstra(src);
-    for(auto& it : dist) {
-        cout << it << " ";
+    path.push_back(src);
+    reverse(path.begin(), path.end());
+    return path;
+}
+
+int32_t main() {
+    ios_base::sync_with_stdio(false); 
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < m; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        g[u].push_back({v, w});
+        g[v].push_back({u, w});
+    }
+    Dijkstra(1);
+    if(dist[n] == INF) {
+        cout << -1 << "\n";
+    }else {
+        auto path = get_path(1, n);
+        for(auto& v : path) {
+            cout << v << " ";
+        }
     }
 
     return 0;
